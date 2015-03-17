60fceedf4102dc313d7c9745c226f1d3c65be34e
Josh Owens <joshua.owens@gmail.com>
2015-03-17 15:29:43 -0400

    Add events for form input. Finishes Day 6 main goals.

--- client/events.js
+++ client/events.js
@@ -3,3 +3,18 @@ Template.myEvents.helpers({
     return Events.find();
   }
 });
+
+Template.myEvents.events({
+  'submit form': function(event, template) {
+    event.preventDefault();
+    eventName = template.$('#eventName').val();
+    Events.insert({name: eventName}, function(error, id) {
+      if (id) {
+        template.$('#eventName').val(null);
+        CoffeeAlerts.success("Your event has been added.");
+      } else {
+        CoffeeAlerts.warning("There was an error saving your event.");
+      }
+    });
+  }
+});
